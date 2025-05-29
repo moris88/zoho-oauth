22881f1f34089c325acdc44fa973c7c65d1b9599
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2025-05-30 00:03:12 +0200

    fix: eslint error

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
 dotenv.config() // carica le variabili d'ambiente dal file .env
 
